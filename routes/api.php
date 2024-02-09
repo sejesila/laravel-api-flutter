535e48cd466a4a2e2595bf16940af5fdd53898bf
sejesila <silaseje5@gmail.com>
2024-02-09 09:30:31 +0300

    Show() with RouteModelBinding and Route::apiResource

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('categories', [CategoryController::class,'index']);
+Route::apiResource('categories',CategoryController::class);
