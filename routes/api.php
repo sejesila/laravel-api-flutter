ccfc667ebe473392bc72ae927e50e506f74ceaf3
sejesila <silaseje5@gmail.com>
2024-02-12 09:40:00 +0300

    Sanctum Auth: Generate/Use Tokens

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('categories',CategoryController::class);
-Route::apiResource('transactions',TransactionController::class);
+
+Route::group(['middleware'=> 'auth:sanctum'],function (){
+    Route::apiResource('categories',CategoryController::class);
+    Route::apiResource('transactions',TransactionController::class);
+});
+Route::post('/tokens/create', function (Request $request) {
+    $user = User::find(1);
+    $token = $user->createToken($user->email);
+    return ['token' => $token->plainTextToken];
+});
+
